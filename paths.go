d5d4445e84ec64df450b4a20406ae94399059a96
abhinav3256 <abhinavkumar3256@gmail.com>
2022-06-21 16:59:30 +0530

    adding file

---
+++ paths.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+var arr = [][]string{{"Lima", "Sao Paulo"}, {"London", "New York"}, {"New York", "Lima"}}
+
+func main() {
+	m := make(map[string]string)
+	for i := 0; i < len(arr); i++ {
+
+		path1 := arr[i][0]
+
+		m[path1] = arr[i][1]
+
+	}
+
+	for key, value := range m {
+
+		temp := value
+
+		for key2, _ := range m {
+
+			if temp == key2 {
+				delete(m, key)
+				break
+			}
+
+		}
+
+	}
+	for key3, _ := range m {
+		fmt.Println(m[key3])
+	}
+
+}
